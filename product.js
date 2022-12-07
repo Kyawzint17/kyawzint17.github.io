43efbd3681ba52d40257c8662611c714a9d7ad95
Kyawzint17 <u6228105@au.edu>
2022-12-07 18:55:42 +0700

    Product Table

---
+++ product.js
@@ -0,0 +1,62 @@
+//define data
+var products = [
+    {
+        name: "Samsung GS",
+        quantity: 1,
+        ppu: 15000,
+    },
+    {
+        name: "Xiaomi Xbox",
+        quantity: 2,
+        ppu: 500
+    },
+    {
+        name: "iPhone 10 Pro Max Ultra",
+        quantity: 1,
+        ppu: 50000
+    },
+    {
+        name:"ASUS Gaming Mouse",
+        quantity: 1,
+        ppu: 4000
+    }
+]
+
+function loadData() {
+
+    let productList = document.getElementById("productList")
+
+    for (let p in products) {
+         let row = document.createElement("tr")
+         let productName = document.createElement("td")
+         productName.innerHTML = products[p].name
+
+         let quantity = document.createElement("td")
+         quantity.innerHTML = products[p].quantity
+         quantity.classList.add("text-center")
+
+         let ppu = document.createElement("td")
+         ppu.innerHTML = products[p].ppu
+         ppu.classList.add("text-right")
+
+         let total = document.createElement("td")
+         total.innerHTML = products[p].ppu * products[p].quantity
+         total.classList.add("text-right")
+         
+         row.appendChild(productName)
+         row.appendChild(quantity)
+         row.appendChild(ppu)
+         row.appendChild(total)
+         productList.appendChild(row)
+    }
+
+    let gross = products.map(products=>products.ppu).reduce((prev, cur) => prev + cur);
+
+    document.getElementById("gross").innerHTML = gross
+
+    let vat = gross * 0.07
+    let net = gross + vat
+    document.getElementById("vat").innerHTML = vat.toFixed(2)
+    document.getElementById("net").innerHTML = net.toFixed(2)
+    
+}
